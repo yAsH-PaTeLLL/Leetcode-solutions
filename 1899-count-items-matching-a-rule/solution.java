36c9834f8e02b082276f015df6627ef0c52789c9
Yash Patel <yashpatelin2005@gmail.com>
2026-05-20 18:19:46 +0000

    Sync LeetCode submission Runtime - 4 ms (97.18%), Memory - 49.6 MB (66.57%)

---
+++ 1899-count-items-matching-a-rule/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+
+        int count = 0;
+
+        int c;
+
+        if(ruleKey.equals("type")) {
+            c = 0;
+        }
+
+        else if(ruleKey.equals("color")) {
+            c = 1;
+        }
+
+        else {
+            c = 2;
+        }
+
+        for(int i = 0; i < items.size(); i++) {
+
+            if(items.get(i).get(c).equals(ruleValue)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
