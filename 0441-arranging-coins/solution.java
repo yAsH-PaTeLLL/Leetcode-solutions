a34853faca980c0c2457a4c0db5a663c1eb59352
Yash Patel <yashpatelin2005@gmail.com>
2026-05-28 16:12:11 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 42.8 MB (43.84%)

---
+++ 0441-arranging-coins/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int arrangeCoins(int n) {
+       long start=0;
+       long end=n;
+       
+        while(start<=end){
+            long mid=start+(end -start)/2;
+            long coins= mid*(mid+1)/2;
+            if(coins==n){
+                
+                return  (int) mid;
+            }
+            
+            if(coins>n){
+            end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+            
+        
+        }
+    return (int)end;
+    }
+}
