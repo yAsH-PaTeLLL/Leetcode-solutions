2051d6b045c1bf9171112e8c1eeea90854702b85
Yash Patel <yashpatelin2005@gmail.com>
2026-05-30 20:59:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.7 MB (84.14%)

---
+++ 0941-sort-array-by-parity/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int i=0,j=0;
+        while(i<nums.length){
+            if(nums[i]%2==0){
+                 int temp=nums[i];
+                 nums[i]=nums[j];
+                 nums[j]=temp;
+                 j++;
+            }
+            i++;
+        }
+    return nums;
+    }
+}
