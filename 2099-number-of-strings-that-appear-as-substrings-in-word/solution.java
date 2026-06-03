101610f302a42a7b7c1c9164f81e2176092a031f
Yash Patel <yashpatelin2005@gmail.com>
2026-06-03 09:01:03 +0000

    Sync LeetCode submission Runtime - 1 ms (76.38%), Memory - 43.3 MB (75.75%)

---
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numOfStrings(String[] patterns, String word) {
+    int count=0;
+    for(String str:patterns){
+        if(word.contains(str)){
+            count++;
+        }
+    }
+
+
+    return count;
+    }
+}
