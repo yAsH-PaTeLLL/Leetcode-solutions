1109d960eeab45816f1711b76e602adc81e24a47
Yash Patel <yashpatelin2005@gmail.com>
2026-06-08 12:34:32 +0000

    Sync LeetCode submission Runtime - 163 ms (5.81%), Memory - 47.2 MB (5.02%)

---
+++ 0020-valid-parentheses/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        while(
+            s.contains("()") ||
+            s.contains("{}") ||
+            s.contains("[]")
+        ){
+            s = s.replace("()", "")
+                 .replace("{}", "")
+                 .replace("[]", "");
+        }
+
+        return s.isEmpty();
+    }
+}
