4b760fd06b8ecfce054ad844c4d30d9c544ae6f9
Yash Patel <yashpatelin2005@gmail.com>
2026-06-14 20:11:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.5 MB (40.80%)

--- 0168-excel-sheet-column-title/solution.java
+++ 0168-excel-sheet-column-title/solution.java
@@ -1,17 +1,13 @@
 class Solution {
     public String convertToTitle(int columnNumber) {
-        if(columnNumber<=26){
-            char num=((char)('A'+columnNumber-1));
-            return ""+num;
-        }
-        StringBuilder sb=new StringBuilder();
-        if(columnNumber>26){
-            while(columnNumber>0){
+        StringBuilder s= new StringBuilder();
+        int n=0;
+        while(columnNumber!=0){
                 columnNumber--;
-            sb.append((char)('A'+columnNumber%26 ) );
-            columnNumber=columnNumber/26;}
-            return sb.reverse().toString();
+                n=columnNumber%26;
+                s.append((char)(n+'A'));
+                  columnNumber=columnNumber/26;          
         }
-    return "";
+    return s.reverse().toString();
     }
 }
